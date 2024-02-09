86ff8ef319f0dc4e2086a579db6acce14182a664
Kruize Derek Christensen <christensen.kruize@gmail.com>
2024-02-09 10:26:20 -0700

    Add files via upload

---
+++ COBOL/HELLO.cbl
@@ -0,0 +1,10 @@
+      *-----------------------
+      * Copyright Contributors to the COBOL Programming Course
+      * SPDX-License-Identifier: CC-BY-4.0
+      *-----------------------
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+       PROCEDURE DIVISION.
+           DISPLAY 'HELLO WORLD!'.
+           GOBACK.
+
